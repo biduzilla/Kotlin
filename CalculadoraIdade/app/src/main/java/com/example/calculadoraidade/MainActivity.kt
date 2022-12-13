652430052899686b9926dd2a551ec340d60bb523
biduzilla <rlyowlly19@gmail.com>
2022-12-13 11:47:54 -0300

    pop up calendario

--- CalculadoraIdade/app/src/main/java/com/example/calculadoraidade/MainActivity.kt
+++ CalculadoraIdade/app/src/main/java/com/example/calculadoraidade/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.calculadoraidade
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById(R.id.btnDatePicker)
+
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker() {
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(
+            this,
+            { view, year, month, dayOfMonth ->
+                Toast.makeText(
+                    this,
+                    "apertou",
+                    Toast.LENGTH_LONG
+                ).show()
+            },
+            year, month, day
+
+        ).show()
+
+
     }
 }
