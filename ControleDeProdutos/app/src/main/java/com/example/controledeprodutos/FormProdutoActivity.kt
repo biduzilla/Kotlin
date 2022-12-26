db2c29117a6cf3c3042e18ea5a62e6ddecaa7efd
biduzilla <rlyowlly19@gmail.com>
2022-12-26 14:26:48 -0300

    botao voltar e mudança texto botão

--- ControleDeProdutos/app/src/main/java/com/example/controledeprodutos/FormProdutoActivity.kt
+++ ControleDeProdutos/app/src/main/java/com/example/controledeprodutos/FormProdutoActivity.kt
@@ -1,8 +1,10 @@
 package com.example.controledeprodutos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
@@ -12,6 +14,7 @@ class FormProdutoActivity : AppCompatActivity() {
     private var editValor: EditText? = null
     private var produtoDAO: ProdutoDAO? = null
     private var produto = ProdutoEntity()
+    private var btnForm:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +24,18 @@ class FormProdutoActivity : AppCompatActivity() {
         editProduto = findViewById(R.id.edit_produto)
         editQuantidade = findViewById(R.id.edit_quantidade)
         editValor = findViewById(R.id.edit_valor)
-
+        btnForm =findViewById(R.id.btn_form)
         val bundle: Bundle? = intent.extras
         if (bundle != null) {
             produto = bundle.getSerializable("produto") as ProdutoEntity
+            btnForm!!.text = "atualizar"
             editProduto()
         }
+    }
+
+    fun voltarHome(view:View){
+        startActivity(Intent(this,MainActivity::class.java))
+
     }
 
     private fun editProduto() {
