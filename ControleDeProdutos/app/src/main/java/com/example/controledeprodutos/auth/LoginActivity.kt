be578a36af79735f1d67b168a4f3d731d9008e15
biduzilla <rlyowlly19@gmail.com>
2022-12-29 15:49:03 -0300

    login firebase

--- ControleDeProdutos/app/src/main/java/com/example/controledeprodutos/auth/LoginActivity.kt
+++ ControleDeProdutos/app/src/main/java/com/example/controledeprodutos/auth/LoginActivity.kt
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import com.example.controledeprodutos.R
+import com.example.controledeprodutos.activity.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var editEmail: EditText
     private lateinit var editSenha: EditText
     private lateinit var auth: FirebaseAuth
+    private lateinit var progressBar: ProgressBar
 
     private var tvCriarConta: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +27,27 @@ class LoginActivity : AppCompatActivity() {
         configClicks()
     }
 
+    private fun validaLogin(email: String, senha: String) {
+        auth.signInWithEmailAndPassword(email, senha)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    val error: String = it.exception!!.message.toString()
+                    Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+                    progressBar.visibility = View.GONE
+                }
+            }
+    }
+
     fun logar(view: View) {
         val email: String = editEmail.text.toString().trim()
         val senha: String = editSenha.text.toString()
 
         if (email.isNotEmpty()) {
             if (senha.isNotEmpty()) {
-                Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show()
+                progressBar.visibility = View.VISIBLE
+                validaLogin(email, senha)
             } else {
                 editSenha.requestFocus()
                 editSenha.error = "Digite sua Senha"
@@ -48,6 +65,8 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initComponets() {
+        auth = FirebaseAuth.getInstance()
+        progressBar = findViewById(R.id.progressBar)
         editEmail = findViewById(R.id.edit_email)
         editSenha = findViewById(R.id.edit_senha)
         tvCriarConta = findViewById(R.id.tv_criar_conta)
