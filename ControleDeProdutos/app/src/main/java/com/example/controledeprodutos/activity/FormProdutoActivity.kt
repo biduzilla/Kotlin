f941042ec180ff3c737af15c0f3bc0fe0a7dc1f4
biduzilla <rlyowlly19@gmail.com>
2023-01-02 10:50:26 -0300

    carregar imagem do dispositivo

--- ControleDeProdutos/app/src/main/java/com/example/controledeprodutos/activity/FormProdutoActivity.kt
+++ ControleDeProdutos/app/src/main/java/com/example/controledeprodutos/activity/FormProdutoActivity.kt
@@ -1,21 +1,27 @@
 package com.example.controledeprodutos.activity
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.controledeprodutos.ProdutoDAO
 import com.example.controledeprodutos.models.ProdutoEntity
 import com.example.controledeprodutos.R
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
-import java.util.jar.Manifest
 
 class FormProdutoActivity : AppCompatActivity() {
     private var editProduto: EditText? = null
@@ -26,13 +32,42 @@ class FormProdutoActivity : AppCompatActivity() {
     private var btnForm: Button? = null
     private var ivImgProduto: ImageView? = null
     private val REQUEST_GALERIA = 100
+    private var caminhoImage: String? = null
+    private var image: Bitmap? = null
+    private var resultLauncher:ActivityResultLauncher<Intent>? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_produto)
-
+        carregaImagem()
         initComponents()
     }
+
+    private fun carregaImagem(){
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = it.data
+                val localImageSelect: Uri? = data?.data
+                caminhoImage = localImageSelect.toString()
+
+                image = if (Build.VERSION.SDK_INT < 28) {
+                    MediaStore.Images.Media.getBitmap(
+                        baseContext.contentResolver,
+                        localImageSelect
+                    )
+                } else {
+                    val source: ImageDecoder.Source = ImageDecoder.createSource(
+                        baseContext.contentResolver,
+                        localImageSelect!!
+                    )
+                    ImageDecoder.decodeBitmap(source)
+                }
+                Log.i("Caminho Imagem", caminhoImage!!)
+            }
+        }
+    }
+
 
     private fun initComponents() {
         produtoDAO = ProdutoDAO(this)
@@ -51,35 +86,32 @@ class FormProdutoActivity : AppCompatActivity() {
 
     fun voltarHome(view: View) {
         startActivity(Intent(this, MainActivity::class.java))
-
     }
 
-    fun abrirGaleira(view: View) {
-        verificaPermicaoGaleira()
-    }
-
-    private fun verificaPermicaoGaleira() {
+    fun verificaPermicaoGaleira(view: View) {
         val permission: PermissionListener = object : PermissionListener {
             override fun onPermissionGranted() {
                 openGaleria()
             }
 
             override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
-                Toast.makeText(this@FormProdutoActivity, "Galeria", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@FormProdutoActivity, "Galeria Error", Toast.LENGTH_SHORT).show()
             }
         }
-
         showDialogPermission(permission, listOf(android.Manifest.permission.READ_EXTERNAL_STORAGE))
     }
 
-    private fun openGaleria() {
-        val intent: Intent =
-            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        startActivityForResult(intent, REQUEST_GALERIA)
 
+    private fun openGaleria() {
+        val intent =
+            Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        resultLauncher!!.launch(intent)
+
+        //        startActivityForResult(intent, REQUEST_GALERIA)
     }
 
-    private fun showDialogPermission(listener: PermissionListener, permissoes:List<String>) {
+
+    private fun showDialogPermission(listener: PermissionListener, permissoes: List<String>) {
         TedPermission.create().setPermissionListener(listener)
             .setDeniedMessage("Permissão negada para acessar galeria, deseja permitir?")
             .setDeniedTitle("Permissões")
