373b9b81501c125c37350e972b74a4c78b9cef2e
biduzilla <rlyowlly19@gmail.com>
2023-01-02 17:19:23 -0300

    recuperar senha firebase

--- CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/LoginActivity.kt
+++ CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/LoginActivity.kt
@@ -3,12 +3,22 @@ package com.toddy.casaportemporada.activity.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.toddy.casaportemporada.R
+import com.toddy.casaportemporada.activity.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var tvCriarConta: TextView
     private lateinit var tvRecuperarConta: TextView
+    private lateinit var etEmail: EditText
+    private lateinit var etSenha: EditText
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -29,7 +39,43 @@ class LoginActivity : AppCompatActivity() {
 
 
     private fun initComponents() {
+        progressBar = findViewById(R.id.progress_bar)
+        etEmail = findViewById(R.id.et_email)
+        etSenha = findViewById(R.id.et_senha)
         tvCriarConta = findViewById(R.id.tv_criar_conta)
         tvRecuperarConta = findViewById(R.id.tv_tv_recuperar_conta)
+    }
+
+    fun validaDados(view: View) {
+        val email: String = etEmail.text.toString()
+        val senha: String = etSenha.text.toString()
+
+        when {
+            email.isEmpty() -> {
+                etEmail.requestFocus()
+                etEmail.error = "Infome um email"
+            }
+            senha.isEmpty() -> {
+                etSenha.requestFocus()
+                etSenha.error = "Infome um email"
+            }
+            else -> {
+                progressBar.visibility = View.VISIBLE
+                logar(email, senha)
+            }
+        }
+    }
+
+    private fun logar(email: String, senha: String) {
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if (it.isSuccessful) {
+                finish()
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                val error: String? = it.exception?.message
+                Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/RecuperarContaActivity.kt
+++ CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/RecuperarContaActivity.kt
@@ -2,13 +2,20 @@ package com.toddy.casaportemporada.activity.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.toddy.casaportemporada.R
 
 class RecuperarContaActivity : AppCompatActivity() {
     private lateinit var tvTituloToolBar: TextView
+    private lateinit var etEmail:EditText
     private lateinit var ibVoltar: ImageButton
+    private lateinit var progressBar: ProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recuperar_conta)
@@ -20,9 +27,36 @@ class RecuperarContaActivity : AppCompatActivity() {
     private fun initComponets() {
         tvTituloToolBar = findViewById(R.id.tv_titulo_toolbar_voltar)
         tvTituloToolBar.text = "Recuperar Conta"
-
+        etEmail = findViewById(R.id.et_email_recuperar)
+        progressBar = findViewById(R.id.progress_bar)
         ibVoltar = findViewById(R.id.ib_toolbar_voltar)
     }
+
+    fun validaDados(view: View){
+        val email:String = etEmail.text.toString()
+
+        if (email.isNotEmpty()){
+            recuperarConta(email)
+            progressBar.visibility = View.VISIBLE
+        }else{
+            etEmail.requestFocus()
+            etEmail.error = "Informe um email"
+        }
+    }
+
+    private fun recuperarConta(email:String){
+        val auth = FirebaseAuth.getInstance()
+        auth.sendPasswordResetEmail(email).addOnCompleteListener{
+            if (it.isSuccessful){
+                Toast.makeText(this, "Email enviado com sucesso", Toast.LENGTH_SHORT).show()
+            }else{
+                val error:String = it.exception!!.message.toString()
+                Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+            }
+            progressBar.visibility =View.GONE
+        }
+    }
+
 
     private fun configClicks() {
         ibVoltar.setOnClickListener {
