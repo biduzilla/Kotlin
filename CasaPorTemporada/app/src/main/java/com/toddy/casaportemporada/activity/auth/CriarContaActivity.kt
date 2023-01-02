6b6520b19da4053554cfe9b99eaece175025c946
biduzilla <rlyowlly19@gmail.com>
2023-01-02 16:05:23 -0300

    layout criar conta

---
+++ CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/CriarContaActivity.kt
@@ -0,0 +1,12 @@
+package com.toddy.casaportemporada.activity.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toddy.casaportemporada.R
+
+class CriarContaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_criar_conta)
+    }
+}
--- CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/LoginActivity.kt
+++ CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/LoginActivity.kt
@@ -1,12 +1,30 @@
 package com.toddy.casaportemporada.activity.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.toddy.casaportemporada.R
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var tvCriarConta: TextView
+    private lateinit var tvRecuperarConta: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        initComponents()
+        configClicks()
+    }
+
+    private fun configClicks() {
+        tvCriarConta.setOnClickListener {
+            startActivity(Intent(this, CriarContaActivity::class.java))
+        }
+    }
+
+    private fun initComponents() {
+        tvCriarConta = findViewById(R.id.tv_criar_conta)
+        tvRecuperarConta = findViewById(R.id.tv_tv_recuperar_conta)
     }
 }
