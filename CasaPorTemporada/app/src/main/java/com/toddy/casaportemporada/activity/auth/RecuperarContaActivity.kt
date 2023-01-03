e48df366af5cb7bd3e438cca42234494bb8e2afd
biduzilla <rlyowlly19@gmail.com>
2023-01-03 09:32:56 -0300

    layout form anuncio

--- CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/RecuperarContaActivity.kt
+++ CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/auth/RecuperarContaActivity.kt
@@ -13,9 +13,10 @@ import com.toddy.casaportemporada.R
 
 class RecuperarContaActivity : AppCompatActivity() {
     private lateinit var tvTituloToolBar: TextView
-    private lateinit var etEmail:EditText
+    private lateinit var etEmail: EditText
     private lateinit var ibVoltar: ImageButton
     private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recuperar_conta)
@@ -32,36 +33,34 @@ class RecuperarContaActivity : AppCompatActivity() {
         ibVoltar = findViewById(R.id.ib_toolbar_voltar)
     }
 
-    fun validaDados(view: View){
-        val email:String = etEmail.text.toString()
+    fun validaDados(view: View) {
+        val email: String = etEmail.text.toString()
 
-        if (email.isNotEmpty()){
+        if (email.isNotEmpty()) {
             recuperarConta(email)
             progressBar.visibility = View.VISIBLE
-        }else{
+        } else {
             etEmail.requestFocus()
             etEmail.error = "Informe um email"
         }
     }
 
-    private fun recuperarConta(email:String){
+    private fun recuperarConta(email: String) {
         val auth = FirebaseAuth.getInstance()
-        auth.sendPasswordResetEmail(email).addOnCompleteListener{
-            if (it.isSuccessful){
+        auth.sendPasswordResetEmail(email).addOnCompleteListener {
+            if (it.isSuccessful) {
                 Toast.makeText(this, "Email enviado com sucesso", Toast.LENGTH_SHORT).show()
-            }else{
-                val error:String = it.exception!!.message.toString()
+            } else {
+                val error: String = it.exception!!.message.toString()
                 Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
             }
-            progressBar.visibility =View.GONE
+            progressBar.visibility = View.GONE
         }
     }
-
 
     private fun configClicks() {
         ibVoltar.setOnClickListener {
             finish()
         }
     }
-
 }
