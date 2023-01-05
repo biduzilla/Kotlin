5a7652574720e2867be072875e978d4376742f45
biduzilla <rlyowlly19@gmail.com>
2023-01-05 10:51:09 -0300

    layout tela anuncio

---
+++ CasaPorTemporada/app/src/main/java/com/toddy/casaportemporada/activity/DetalheAnuncioActivity.kt
@@ -0,0 +1,12 @@
+package com.toddy.casaportemporada.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toddy.casaportemporada.R
+
+class DetalheAnuncioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhe_anuncio)
+    }
+}
