2925c468ff667168595489e95d37d99c6c606665
biduzilla <rlyowlly19@gmail.com>
2023-01-06 14:16:04 -0300

    layout recuperar senha

---
+++ GerenciadorReceitas/app/src/main/java/com/toddy/gerenciadorreceitas/activity/auth/EsqueciSenhaActivity.kt
@@ -0,0 +1,12 @@
+package com.toddy.gerenciadorreceitas.activity.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.toddy.gerenciadorreceitas.R
+
+class EsqueciSenhaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_esqueci_senha)
+    }
+}
