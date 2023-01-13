0a7279171107736c78ff78e18018e56ccb08f08e
biduzilla <rlyowlly19@gmail.com>
2023-01-13 13:59:24 -0300

    navigation botton

--- OLXClone/app/src/main/java/com/toddy/olxclone/MainActivity.kt
+++ OLXClone/app/src/main/java/com/toddy/olxclone/MainActivity.kt
@@ -2,10 +2,19 @@ package com.toddy.olxclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottonNavView: BottomNavigationView = findViewById(R.id.navigation_view)
+        val navController:NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(bottonNavView,navController)
     }
 }
