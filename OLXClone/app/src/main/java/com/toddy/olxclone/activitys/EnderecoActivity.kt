be2b2703a2240bcca161d8280535bb4a7d3d6131
biduzilla <rlyowlly19@gmail.com>
2023-01-16 09:47:25 -0300

    validação dados endereço

--- OLXClone/app/src/main/java/com/toddy/olxclone/activitys/EnderecoActivity.kt
+++ OLXClone/app/src/main/java/com/toddy/olxclone/activitys/EnderecoActivity.kt
@@ -2,11 +2,64 @@ package com.toddy.olxclone.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.toddy.olxclone.R
 
 class EnderecoActivity : AppCompatActivity() {
+    private lateinit var etCep:EditText
+    private lateinit var etUf:EditText
+    private lateinit var etMunicipio:EditText
+    private lateinit var etBairro:EditText
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_endereco)
+
+        initComponents()
+    }
+
+    fun validaDados(view: View){
+        val cep:String = etCep.text.toString()
+        val uf:String = etUf.text.toString()
+        val municipio:String = etMunicipio.text.toString()
+        val bairro:String = etBairro.text.toString()
+
+        when{
+            cep.isEmpty() -> {
+                etCep.requestFocus()
+                etCep.error = "Campo Obrigatório"
+            }
+            uf.isEmpty() -> {
+                etUf.requestFocus()
+                etUf.error = "Campo Obrigatório"
+            }
+            municipio.isEmpty() -> {
+                etMunicipio.requestFocus()
+                etMunicipio.error = "Campo Obrigatório"
+            }
+            bairro.isEmpty() -> {
+                etBairro.requestFocus()
+                etBairro.error = "Campo Obrigatório"
+            }
+            else -> {
+                progressBar.visibility = View.VISIBLE
+            }
+        }
+
+    }
+
+    private fun initComponents(){
+        val titulo:TextView = findViewById(R.id.text_toolbar)
+        titulo.text = "Endereço"
+
+        etCep = findViewById(R.id.et_cep)
+        etUf = findViewById(R.id.et_uf)
+        etMunicipio = findViewById(R.id.et_cidade)
+        etBairro = findViewById(R.id.et_bairro)
+        progressBar = findViewById(R.id.progress_bar)
     }
 }
