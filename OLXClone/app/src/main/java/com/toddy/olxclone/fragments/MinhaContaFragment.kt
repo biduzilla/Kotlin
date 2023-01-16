8a6b6862d4ad001288c3946956dbae5ecc02bb8c
biduzilla <rlyowlly19@gmail.com>
2023-01-16 09:35:38 -0300

    fragment minha conta ir endereço e perfil

--- OLXClone/app/src/main/java/com/toddy/olxclone/fragments/MinhaContaFragment.kt
+++ OLXClone/app/src/main/java/com/toddy/olxclone/fragments/MinhaContaFragment.kt
@@ -1,17 +1,38 @@
 package com.toddy.olxclone.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.toddy.olxclone.R
+import com.toddy.olxclone.activitys.EnderecoActivity
+import com.toddy.olxclone.activitys.PerfilActivity
+
 class MinhaContaFragment : Fragment() {
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_minha_conta, container, false)
+
+        val view: View = inflater.inflate(R.layout.fragment_minha_conta, container, false)
+
+        configClicks(view)
+
+        return view
+    }
+
+    private fun configClicks(view: View) {
+        view.findViewById<ConstraintLayout>(R.id.menu_perfil).setOnClickListener {
+            startActivity(Intent(view.context, PerfilActivity::class.java))
+        }
+
+        view.findViewById<ConstraintLayout>(R.id.menu_endereco).setOnClickListener {
+            startActivity(Intent(view.context, EnderecoActivity::class.java))
+        }
     }
 }
