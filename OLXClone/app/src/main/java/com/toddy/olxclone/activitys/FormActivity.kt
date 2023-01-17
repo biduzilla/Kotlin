a15321d051d6c9b88feb9edf1ede64c0025d0831
biduzilla <rlyowlly19@gmail.com>
2023-01-17 10:21:14 -0300

    mascara R$

--- OLXClone/app/src/main/java/com/toddy/olxclone/activitys/FormActivity.kt
+++ OLXClone/app/src/main/java/com/toddy/olxclone/activitys/FormActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
+import com.blackcat.currencyedittext.CurrencyEditText
 import com.toddy.olxclone.R
+import java.util.*
 
 class FormActivity : AppCompatActivity() {
-    private lateinit var tituloToolbar:TextView
-    private lateinit var ibVoltar:ImageButton
+    private lateinit var tituloToolbar: TextView
+    private lateinit var ibVoltar: ImageButton
+    private lateinit var etPreco: CurrencyEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,14 +19,17 @@ class FormActivity : AppCompatActivity() {
 
         initComponents()
     }
-    private fun initComponents(){
+
+    private fun initComponents() {
         tituloToolbar = findViewById(R.id.text_toolbar)
         tituloToolbar.text = "Novo Anúncio"
 
         ibVoltar = findViewById(R.id.ib_voltar)
+        etPreco = findViewById(R.id.et_preco)
+        etPreco.locale = Locale("PT", "br")
     }
 
-    private fun configClicks(){
+    private fun configClicks() {
         ibVoltar.setOnClickListener {
             finish()
         }
