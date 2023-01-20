c5c51e7f5f8212250057a91bd9e66e3d80d62d1a
biduzilla <rlyowlly19@gmail.com>
2023-01-20 09:45:18 -0300

    recuperação endereço

--- OLXClone/app/src/main/java/com/toddy/olxclone/activitys/FormActivity.kt
+++ OLXClone/app/src/main/java/com/toddy/olxclone/activitys/FormActivity.kt
@@ -4,15 +4,23 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.widget.addTextChangedListener
 import com.blackcat.currencyedittext.CurrencyEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.toddy.olxclone.R
 import com.toddy.olxclone.model.Categoria
+import com.toddy.olxclone.model.Endereco
+import org.w3c.dom.Text
 import java.util.*
 
 class FormActivity : AppCompatActivity() {
@@ -20,11 +28,13 @@ class FormActivity : AppCompatActivity() {
     private lateinit var ibVoltar: ImageButton
     private lateinit var etPreco: CurrencyEditText
     private lateinit var btnCategoria: Button
+    private lateinit var etCep: EditText
 
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     private var categoria = Categoria()
-    private var categoriaSelecionada:String? = null
+    private var endereco = Endereco()
+    private var categoriaSelecionada: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +43,56 @@ class FormActivity : AppCompatActivity() {
         initComponents()
         configClicks()
         startResult()
+        recuperaEndereco()
     }
 
     private fun startResult() {
         resultLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
                 if (it.resultCode == Activity.RESULT_OK) {
-                    val data:Intent? = it.data
+                    val data: Intent? = it.data
                     categoria = it.data!!.getSerializableExtra("categoriaSelecionada") as Categoria
                     categoriaSelecionada = categoria.nome
                     btnCategoria.text = categoriaSelecionada
-                }else{
+                } else {
 
                 }
             }
+    }
+
+    private fun recuperaEndereco() {
+        configCep()
+        FirebaseDatabase.getInstance().reference
+            .child("enderecos")
+            .child(FirebaseAuth.getInstance().currentUser!!.uid)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    endereco = snapshot.getValue(Endereco::class.java)!!
+                    etCep.setText(endereco.cep)
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+    }
+
+    private fun configCep() {
+        etCep.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+               Toast.makeText(this@FormActivity, "Endereço recuperado", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
     }
 
     fun selecionarCategoria(view: View) {
@@ -60,6 +106,7 @@ class FormActivity : AppCompatActivity() {
 
         btnCategoria = findViewById(R.id.btn_categorias)
         ibVoltar = findViewById(R.id.ib_voltar)
+        etCep = findViewById(R.id.et_cep)
 
         etPreco = findViewById(R.id.et_preco)
         etPreco.locale = Locale("PT", "br")
