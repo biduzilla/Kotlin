1a1a4fd5cda00e56714d3eef654c6a19b8194fb0
biduzilla <rlyowlly19@gmail.com>
2023-01-23 11:35:36 -0300

    carregando img camera

--- OLXClone/app/src/main/java/com/toddy/olxclone/activitys/FormActivity.kt
+++ OLXClone/app/src/main/java/com/toddy/olxclone/activitys/FormActivity.kt
@@ -9,6 +9,7 @@ import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
@@ -17,6 +18,7 @@ import android.view.View
 import android.widget.*
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.core.widget.addTextChangedListener
 import com.blackcat.currencyedittext.CurrencyEditText
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -37,6 +39,9 @@ import okhttp3.Request
 import org.w3c.dom.Text
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
 import java.util.*
 
 class FormActivity : AppCompatActivity() {
@@ -57,6 +62,8 @@ class FormActivity : AppCompatActivity() {
 
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
     private var REQUEST_CODE: Int = 0
+    private lateinit var currentPhotoPath: String
+
     private var retrofit: Retrofit? = null
 
     private var categoria = Categoria()
@@ -130,7 +137,7 @@ class FormActivity : AppCompatActivity() {
     private fun verificaPermissaoCamera(requestCode: Int) {
         val permissionListener: PermissionListener = object : PermissionListener {
             override fun onPermissionGranted() {
-                abriCamera(requestCode)
+                dispatchTakePictureIntent(requestCode)
             }
 
             override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
@@ -169,10 +176,6 @@ class FormActivity : AppCompatActivity() {
         resultLauncher.launch(intent)
     }
 
-    private fun abriCamera(requestCode: Int) {
-
-    }
-
     private fun showDialogPermission(
         permissionListener: PermissionListener,
         permissoes: List<String>,
@@ -193,8 +196,11 @@ class FormActivity : AppCompatActivity() {
                     val bitmap0: Bitmap
                     val bitmap1: Bitmap
                     val bitmap2: Bitmap
+                    var imagemSelecionada: Uri? = null
 
-                    var imagemSelecionada: Uri = it.data?.data!!
+                    it.data?.data.let {uri ->
+                        imagemSelecionada = uri
+                    }
                     val caminhoImagem: String
                     when {
                         REQUEST_CODE <= 2 -> {
@@ -209,7 +215,7 @@ class FormActivity : AppCompatActivity() {
                                     } else {
                                         val source: ImageDecoder.Source = ImageDecoder.createSource(
                                             contentResolver,
-                                            imagemSelecionada
+                                            imagemSelecionada!!
                                         )
                                         ImageDecoder.decodeBitmap(source)
                                     }
@@ -224,7 +230,7 @@ class FormActivity : AppCompatActivity() {
                                     } else {
                                         val source: ImageDecoder.Source = ImageDecoder.createSource(
                                             contentResolver,
-                                            imagemSelecionada
+                                            imagemSelecionada!!
                                         )
                                         ImageDecoder.decodeBitmap(source)
                                     }
@@ -239,7 +245,7 @@ class FormActivity : AppCompatActivity() {
                                     } else {
                                         val source: ImageDecoder.Source = ImageDecoder.createSource(
                                             contentResolver,
-                                            imagemSelecionada
+                                            imagemSelecionada!!
                                         )
                                         ImageDecoder.decodeBitmap(source)
                                     }
@@ -255,6 +261,21 @@ class FormActivity : AppCompatActivity() {
                                 categoriaSelecionada = categoria.nome
                                 btnCategoria.text = categoriaSelecionada
 
+                            }
+                        }
+                        else -> {
+                            val file: File = File(currentPhotoPath)
+                            caminhoImagem = file.toURI().toString()
+                            when (REQUEST_CODE) {
+                                3 -> {
+                                    ivImg1.setImageURI(Uri.fromFile(file))
+                                }
+                                4 -> {
+                                    ivImg2.setImageURI(Uri.fromFile(file))
+                                }
+                                5 -> {
+                                    ivImg3.setImageURI(Uri.fromFile(file))
+                                }
                             }
                         }
                     }
@@ -426,4 +447,59 @@ class FormActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun dispatchTakePictureIntent(requestCode: Int) {
+
+        when (requestCode) {
+            0 -> {
+                REQUEST_CODE = 3
+            }
+            1 -> {
+                REQUEST_CODE = 4
+            }
+            2 -> {
+                REQUEST_CODE = 5
+            }
+        }
+
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.toddy.olxclone.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    resultLauncher.launch(takePictureIntent)
+                }
+            }
+        }
+    }
+
 }
