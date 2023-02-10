1558ca62a8f46628a3386b85660abe29ff8b6339
biduzilla <rlyowlly19@gmail.com>
2023-02-10 17:51:54 -0300

    primeira parte search view ok

--- OLXClone/app/src/main/java/com/toddy/olxclone/fragments/HomeFragment.kt
+++ OLXClone/app/src/main/java/com/toddy/olxclone/fragments/HomeFragment.kt
@@ -1,15 +1,14 @@
 package com.toddy.olxclone.fragments
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -17,12 +16,13 @@ import com.google.firebase.database.*
 import com.toddy.olxclone.R
 import com.toddy.olxclone.activitys.CategoriasActivity
 import com.toddy.olxclone.activitys.EstadosActivity
+import com.toddy.olxclone.activitys.FiltrosActivity
 import com.toddy.olxclone.activitys.FormActivity
 import com.toddy.olxclone.adapter.AnuncioAdapter
 import com.toddy.olxclone.auth.LoginActivity
+import com.toddy.olxclone.helper.SPFiltro
 import com.toddy.olxclone.model.Anuncio
-import com.tsuryo.swipeablerv.SwipeLeftRightCallback
-import com.tsuryo.swipeablerv.SwipeableRecyclerView
+import com.toddy.olxclone.model.Filtro
 
 class HomeFragment : Fragment(), AnuncioAdapter.OnClickListener {
     private lateinit var anunciosAdapter: AnuncioAdapter
@@ -30,31 +30,97 @@ class HomeFragment : Fragment(), AnuncioAdapter.OnClickListener {
 
     private lateinit var progressBar: ProgressBar
     private lateinit var tvInfo: TextView
-    private lateinit var btnNovoAnuncio:Button
+    private lateinit var btnNovoAnuncio: Button
     private lateinit var rvAnuncios: RecyclerView
-    private lateinit var btnRegiao:Button
-    private lateinit var btnCategoria:Button
-    private lateinit var btnFiltro:Button
+
+    private lateinit var btnRegiao: Button
+    private lateinit var btnCategoria: Button
+    private lateinit var btnFiltro: Button
+
+    private lateinit var searchView: androidx.appcompat.widget.SearchView
+    private lateinit var editSearchView: EditText
+
+    private var filtro = Filtro()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val view:View =  inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
 
         initComponents(view)
         confiRv()
         configClicks(view)
-
-        return view;
+        configSearchView()
+        return view
     }
 
-    private fun configClicks(view:View){
+    private fun configSearchView() {
+        editSearchView = searchView.findViewById(androidx.appcompat.R.id.search_src_text)
+
+        searchView.findViewById<androidx.appcompat.widget.AppCompatImageView>(androidx.appcompat.R.id.search_close_btn)
+            .setOnClickListener {
+                limparPesquisa()
+            }
+
+        searchView.setOnQueryTextFocusChangeListener(object : SearchView.OnQueryTextListener,
+            View.OnFocusChangeListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                SPFiltro.setFiltro(requireActivity(), "pesquisa", p0!!)
+                configFiltros()
+                ocultarTeclado()
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onFocusChange(p0: View?, p1: Boolean) {
+
+            }
+        })
+
+    }
+
+    private fun limparPesquisa() {
+        searchView.clearFocus()
+        editSearchView.setText("")
+        SPFiltro.setFiltro(requireActivity(), "pesquisa", "")
+        configFiltros()
+        ocultarTeclado()
+    }
+
+    private fun ocultarTeclado() {
+        val imm =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+    }
+
+    private fun configFiltros() {
+        filtro = SPFiltro.getFiltro(requireActivity())
+
+        if (filtro.estado!!.regiao.isNotEmpty()) {
+            btnRegiao.text = filtro.estado!!.regiao
+        } else {
+            btnRegiao.text = "Regiões"
+        }
+
+        if (filtro.categoria.isNotEmpty()) {
+            btnRegiao.text = filtro.categoria
+        } else {
+            btnRegiao.text = "Categorias"
+        }
+
+        recuperaAnuncios()
+    }
+
+    private fun configClicks(view: View) {
         btnNovoAnuncio.setOnClickListener {
-            if (FirebaseAuth.getInstance().currentUser != null){
+            if (FirebaseAuth.getInstance().currentUser != null) {
                 startActivity(Intent(activity, FormActivity::class.java))
-            }else{
+            } else {
                 startActivity(Intent(activity, LoginActivity::class.java))
             }
         }
@@ -66,7 +132,16 @@ class HomeFragment : Fragment(), AnuncioAdapter.OnClickListener {
         }
 
         btnRegiao.setOnClickListener {
-            startActivity(Intent(activity,EstadosActivity::class.java))
+            startActivity(Intent(activity, EstadosActivity::class.java))
+        }
+
+        btnFiltro.setOnClickListener {
+            startActivity(
+                Intent(
+                    activity,
+                    FiltrosActivity::class.java
+                )
+            )
         }
     }
 
@@ -75,7 +150,7 @@ class HomeFragment : Fragment(), AnuncioAdapter.OnClickListener {
         recuperaAnuncios()
     }
 
-    private fun initComponents(view:View){
+    private fun initComponents(view: View) {
         progressBar = view.findViewById(R.id.progress_bar)
         tvInfo = view.findViewById(R.id.tv_info)
         rvAnuncios = view.findViewById(R.id.rv_anuncios)
@@ -83,6 +158,7 @@ class HomeFragment : Fragment(), AnuncioAdapter.OnClickListener {
         btnRegiao = view.findViewById(R.id.btn_regiao)
         btnCategoria = view.findViewById(R.id.btn_categorias)
         btnFiltro = view.findViewById(R.id.btn_filtro)
+        searchView = view.findViewById(R.id.search_view)
     }
 
     private fun confiRv() {
@@ -93,36 +169,35 @@ class HomeFragment : Fragment(), AnuncioAdapter.OnClickListener {
     }
 
     private fun recuperaAnuncios() {
-            val anuncioRef: DatabaseReference =
-                FirebaseDatabase.getInstance().reference.child("anuncios_publicos")
+        val anuncioRef: DatabaseReference =
+            FirebaseDatabase.getInstance().reference.child("anuncios_publicos")
 
-
-            anuncioRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        anunciosLst.clear()
-                        tvInfo.text = ""
-                        val children = snapshot.children
-                        children.forEach {
-                            it.getValue(Anuncio::class.java).let { anuncio ->
-                                anunciosLst.add(anuncio!!)
-                            }
+        anuncioRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()) {
+                    anunciosLst.clear()
+                    tvInfo.text = ""
+                    val children = snapshot.children
+                    children.forEach {
+                        it.getValue(Anuncio::class.java).let { anuncio ->
+                            anunciosLst.add(anuncio!!)
                         }
-                        anunciosLst.reverse()
-                        anunciosAdapter.notifyDataSetChanged()
-                        progressBar.visibility = View.GONE
-                    } else {
-                        tvInfo.text = "Nenhum anúncio cadastrado"
                     }
+                    anunciosLst.reverse()
+                    anunciosAdapter.notifyDataSetChanged()
+                    progressBar.visibility = View.GONE
+                } else {
+                    tvInfo.text = "Nenhum anúncio cadastrado"
                 }
+            }
 
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-            })
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     override fun OnClick(anuncio: Anuncio) {
-       Toast.makeText(requireContext(),anuncio.titulo,Toast.LENGTH_SHORT).show()
+        Toast.makeText(requireContext(), anuncio.titulo, Toast.LENGTH_SHORT).show()
     }
 }
