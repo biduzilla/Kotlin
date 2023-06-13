9f900b97a96340e8b9886f27565653c8c9cae105
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-06-13 14:55:31 -0300

    finalização layout produto item

--- DeliveryComposeUi/app/src/main/java/com/example/deliverycomposeui/MainActivity.kt
+++ DeliveryComposeUi/app/src/main/java/com/example/deliverycomposeui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.deliverycomposeui
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,14 +15,20 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Bottom
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.deliverycomposeui.ui.theme.DeliveryComposeUiTheme
 import com.example.deliverycomposeui.ui.theme.Purple200
 import com.example.deliverycomposeui.ui.theme.Purple500
@@ -44,29 +51,53 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(200.dp)
-            .clip(RoundedCornerShape(10.dp))
-    ) {
-        Box(
-            modifier = Modifier
-                .height(100.dp)
-                .fillMaxWidth()
-                .background(brush = Brush.horizontalGradient(colors = listOf(Purple500, Teal200)))
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Imagem do produto",
+    Surface(shape = RoundedCornerShape(15.dp), elevation = 10.dp) {
+        Column(
             Modifier
-                .size(100.dp)
-                .offset(y= (-50).dp)
-                .clip(CircleShape)
-                .align(CenterHorizontally)
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            val imgSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imgSize)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500,
+                                Teal200
+                            )
+                        )
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Imagem do produto",
+                    Modifier
+                        .size(imgSize)
+                        .offset(y = imgSize/2)
+                        .clip(CircleShape)
+                        .align(BottomCenter)
+                )
+            }
+            Spacer(modifier = Modifier.height(imgSize/2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W700,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 15.90",
+                    Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
 
-        )
-        Text(text = "1")
-        Text(text = "2")
     }
 }
