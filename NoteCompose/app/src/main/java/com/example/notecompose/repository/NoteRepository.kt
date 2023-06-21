f768d00f03dd452d6f3e985f9bf4221732ddd2ed
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-06-21 10:14:08 -0300

    note repository

--- NoteCompose/app/src/main/java/com/example/notecompose/repository/NoteRepository.kt
+++ NoteCompose/app/src/main/java/com/example/notecompose/repository/NoteRepository.kt
@@ -1,4 +1,18 @@
 package com.example.notecompose.repository
 
-class NoteRepository {
+import com.example.notecompose.data.NoteDatabase
+import com.example.notecompose.data.dao.NoteDataBaseDao
+import com.example.notecompose.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NoteRepository @Inject constructor(private val noteDatabaseDao: NoteDataBaseDao) {
+    suspend fun addNote(note: Note) = noteDatabaseDao.insertNote(note)
+    suspend fun updateNote(note: Note) = noteDatabaseDao.updateNote(note)
+    suspend fun deleteNote(note: Note) = noteDatabaseDao.deleteNote(note)
+    suspend fun deleteAllNotes() = noteDatabaseDao.deleteAll()
+    fun getAllNotes(): Flow<List<Note>> = noteDatabaseDao.getAll().flowOn(Dispatchers.IO).conflate()
 }
