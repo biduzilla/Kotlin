b94c846d78b782000672a0f3a94a486f28b097dc
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-06-27 10:39:13 -0300

    finalizado repository question

--- QuizCompose/app/src/main/java/com/example/quizcompose/data/DataOrException.kt
+++ QuizCompose/app/src/main/java/com/example/quizcompose/data/DataOrException.kt
@@ -2,6 +2,6 @@ package com.example.quizcompose.data
 
 data class DataOrException<T, Boolean, E : Exception>(
     var data: T? = null,
-    val loading: Boolean? = null,
+    var loading: Boolean? = null,
     var e: E? = null
 )
--- QuizCompose/app/src/main/java/com/example/quizcompose/repository/QuestionRepository.kt
+++ QuizCompose/app/src/main/java/com/example/quizcompose/repository/QuestionRepository.kt
@@ -1,10 +1,28 @@
 package com.example.quizcompose.repository
 
+import android.util.Log
 import com.example.quizcompose.data.DataOrException
 import com.example.quizcompose.model.QuestionItem
 import com.example.quizcompose.network.QuestionApi
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionApi) {
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+
+            if (dataOrException.data.toString().isNotEmpty()) {
+                dataOrException.loading = false
+            }
+        } catch (e: Exception) {
+            dataOrException.e = e
+            Log.i("infoteste", "getAllQuestions: ${dataOrException.e!!.localizedMessage}")
+        }
+
+        return dataOrException
+    }
+
 }
